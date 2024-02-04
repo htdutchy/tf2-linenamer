13af694ee8c6a7bea584f2e6d8ced56cf929a1f7
Thomas Kerkhof <htdutchy@barelyhtml.com>
2024-02-04 14:49:54 +0100

    Uploaded base mod files

---
+++ mod.lua
@@ -0,0 +1,19 @@
+function data()
+return {
+  info = {
+    name = _("HTD - Line namer"),
+    description = _("Automatic line naming script"),
+	tags = {"Script Mod", "Misc"},
+    authors = {
+      {
+        name = "HTDutchy",
+	    role = 'CREATOR',
+      },
+    },
+    minorVersion = 2,
+    severityAdd = "NONE",
+    severityRemove = "NONE",
+    url = "https://steamcommunity.com/sharedfiles/filedetails/?id=2996276657",
+  },
+}
+end
